e58b8c594f7d2cee1c231a2714e6e3e0c0295c51
Santiago Cendales <srcendales94@gmail.com>
2025-04-05 11:55:52 -0500

    Add pie chart component

---
+++ src/Components/EnergyGenerationPieChart.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import { IFuelGenerationData } from '../types';
+import { getEnergyColors } from '../Utils/generateChartColors';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: [],
+  datasets: [
+    {
+      label: [],
+      data: [],
+      borderWidth: 1,
+    },
+  ],
+};
+
+interface EnergyGenerationPieChartProps {
+  energyGenerationData: IFuelGenerationData
+}
+
+export const EnergyGenerationPieChart: React.FC<EnergyGenerationPieChartProps> = ({
+  energyGenerationData
+}) => {
+
+  const colors = getEnergyColors(energyGenerationData.generationmix);
+
+  const data = {
+    labels: energyGenerationData.generationmix.map(data => data.fuel),
+    datasets: [
+      {
+        label: 'Percentage (%)',
+        data: energyGenerationData.generationmix.map(data => data.perc),
+        backgroundColor: colors,
+        borderColor: colors,
+        borderWidth: 1,
+      }
+    ],
+  }
+  return (
+    <div style={{ diplay: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0 300px' }}>
+      <Pie data={data} />
+    </div>
+  )
+}
